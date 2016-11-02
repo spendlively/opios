dfaf8887ef5ced78ae7dfbb257ae2214da06055d
spendlively <spendlively@mail.ru>
2016-11-02 14:07:51 +0700

    Добавил тестовые сервисы

--- httpd/modules/Launcher.js
+++ httpd/modules/Launcher.js
@@ -9,8 +9,26 @@ Launcher.prototype.init = function(){
 		$('.tnb-li.service').click(function(e){
 			$('.tnb-li.service').removeClass('active-btn');
 			$(this).addClass('active-btn');
-			// console.log(123);
-			// alert(1);
+
+			var service = $(this).children('.tnb-text').text();
+			$('.webview').css('display', 'none');
+
+			switch(service){
+				case 'Messenger':
+					$('#foo1').css('display', 'inline-flex');
+					break;
+				case 'Telegram':
+					$('#foo2').css('display', 'inline-flex');
+					break;
+				case 'WhatsApp':
+					$('#foo3').css('display', 'inline-flex');
+					break;
+				case 'Skype':
+					$('#foo4').css('display', 'inline-flex');
+					break;
+				default:
+					break;
+			}
 		});
 	});
 }
