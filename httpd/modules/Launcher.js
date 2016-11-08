90399f4164d7a80fe14c9ef575d347ae9c2e74f7
spendlively <spendlively@mail.ru>
2016-11-08 23:14:36 +0700

    Подстройка размеров wv

--- httpd/modules/Launcher.js
+++ httpd/modules/Launcher.js
@@ -6,6 +6,7 @@ Launcher.prototype.init = function(){
 	
 	$(document).ready(function(){
 
+
 		$('.tnb-li.service').click(function(e){
 			$('.tnb-li.service').removeClass('active-btn');
 			$(this).addClass('active-btn');
@@ -17,23 +18,57 @@ Launcher.prototype.init = function(){
 			switch(service){
 				case 'Messenger':
 					$('#foo1').css('display', 'inline-flex');
+					correctSize('foo1');
 					break;
 				case 'Telegram':
 					$('#foo2').css('display', 'inline-flex');
+					correctSize('foo2');
 					break;
 				case 'WhatsApp':
 					$('#foo3').css('display', 'inline-flex');
+					correctSize('foo3');
 					break;
 				case 'Skype':
 					$('#foo4').css('display', 'inline-flex');
+					correctSize('foo4');
 					break;
 				default:
 					$('#services-list').css('display', 'block');
+					resize();
 					break;
 			}
 		});
 	});
+
+
+
+	$(window).on('resize', function(e){
+
+		resize();
+	});
+}
+
+function correctSize(id){
+
+	var wv = document.getElementById(id);
+    setTimeout(function(){
+        if(wv.style.height === '99%'){
+            wv.style.height = '98%';
+        }
+        else{
+            wv.style.height = '99%';
+        }
+    }, 500);		
+}
+
+function resize(){
+  	var win = $(window),
+  		width = win.width(),
+  		height = win.height();
+  	
+  	$('.opios-webview').css('height', height - 100 + 'px').css('width', '100%');	
 }
 
 module.exports = Launcher;
 
+	
